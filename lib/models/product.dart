0530357d2c19effc4b45b1ebfa86daa92437b8a4
ahmedmahersamy <ahmedmaherelzoomor@outlook.com>
2021-04-03 21:32:35 +0200

    add product screen and model file

---
+++ lib/models/product.dart
@@ -0,0 +1,7 @@
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+}
---
+++ lib/screens/product_overview_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my shop'),
+      ),
+      body: Center(
+        child: Text('let\'s build a shop!'),
+      ),
+    );
+  }
+}
